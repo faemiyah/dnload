ebbb66906d3aed64814f1a37a6d3d85bf20ee7bc
trilkk <10812332+trilkk@users.noreply.github.com>
2024-11-14 20:46:15 +0000

    Fix refresh.sh, allow override to GLES.

--- refresh.sh
+++ refresh.sh
@@ -10,7 +10,7 @@ if [ ! -f "src/dnload.h" ] ; then
   touch src/dnload.h
 fi
 
-python "${DNLOAD}" -v -E src/intro.cpp
+python "${DNLOAD}" -v -E src/intro.cpp $*
 if [ $? -ne 0 ] ; then
   echo "${0}: regenerating symbols failed"
   exit 1
