4816980c1564fef8e82f4e01a7baf46b898ddef2
trilkk <10812332+trilkk@users.noreply.github.com>
2024-12-19 21:45:02 +0200

    Updated CMake config.

--- defaults.cmake
+++ defaults.cmake
@@ -1,5 +1,8 @@
 cmake_minimum_required(VERSION 3.13)
-cmake_policy(SET CMP0167 NEW) # Boost, CMake 3.30
+# Boost, CMake 3.30
+if(CMP0167)
+    cmake_policy(SET CMP0167 NEW)
+endif()
 
 function(check_c_flag flag var)
     include(CheckCCompilerFlag)
